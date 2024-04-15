4c7ce87db8ad6f018aacc82f0c62eafae5ec043a
purp <purpllove@163.com>
2024-04-15 19:19:37 +0800

    pageSetting

---
+++ src/views/management/pageSetting/components/table-action-modal.vue
@@ -0,0 +1,142 @@
+<template>
+  <n-modal v-model:show="modalVisible" preset="card" :title="title" class="w-700px">
+    <n-form ref="formRef" label-placement="left" :label-width="80" :model="formModel" :rules="rules">
+      <n-grid :cols="24" :x-gap="18">
+        <n-form-item-grid-item :span="12" label="名称" path="name">
+          <n-input v-model:value="formModel.name" />
+        </n-form-item-grid-item>
+        <n-form-item-grid-item :span="12" label="描述" path="description">
+          <n-input v-model:value="formModel.description" />
+        </n-form-item-grid-item>
+        <n-form-item-grid-item :span="12" label="网址" path="site">
+          <n-input v-model:value="formModel.site" />
+        </n-form-item-grid-item>
+        <n-form-item-grid-item :span="12" label="图标" path="icon">
+          <n-input v-model:value="formModel.icon" />
+        </n-form-item-grid-item>
+      </n-grid>
+      <n-space class="w-full pt-16px" :size="24" justify="end">
+        <n-button class="w-72px" round @click="closeModal">取消</n-button>
+        <n-button class="w-72px" type="primary" round @click="handleSubmit">确定</n-button>
+      </n-space>
+    </n-form>
+  </n-modal>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, reactive, watch } from 'vue';
+import type { FormInst, FormItemRule } from 'naive-ui';
+import { genderOptions, userStatusOptions } from '@/constants';
+import { addPage } from '@/service';
+import { formRules, createRequiredFormRule } from '@/utils';
+
+export interface Props {
+  /** 弹窗可见性 */
+  visible: boolean;
+  /**
+   * 弹窗类型
+   * add: 新增
+   * edit: 编辑
+   */
+  type?: 'add' | 'edit';
+  /** 编辑的表格行数据 */
+  editData?: ApiPageManagement.Page | null;
+}
+
+export type ModalType = NonNullable<Props['type']>;
+
+defineOptions({ name: 'TableActionModal' });
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'add',
+  editData: null
+});
+
+interface Emits {
+  (e: 'update:visible', visible: boolean): void;
+}
+
+const emit = defineEmits<Emits>();
+
+const modalVisible = computed({
+  get() {
+    return props.visible;
+  },
+  set(visible) {
+    emit('update:visible', visible);
+  }
+});
+const closeModal = () => {
+  modalVisible.value = false;
+};
+
+const title = computed(() => {
+  const titles: Record<ModalType, string> = {
+    add: '添加用户',
+    edit: '编辑用户'
+  };
+  return titles[props.type];
+});
+
+const formRef = ref<HTMLElement & FormInst>();
+
+type FormModel = Pick<ApiPageManagement.Page, 'id' | 'name' | 'description' | 'site' | 'icon'>;
+
+const formModel = reactive<FormModel>(createDefaultFormModel());
+
+// const rules: Record<keyof FormModel, FormItemRule | FormItemRule[]> = {
+//   id: createRequiredFormRule('请输入用户名'),
+//   name: createRequiredFormRule('请输入年龄'),
+//   description: createRequiredFormRule('请选择性别'),
+//   site: formRules.phone,
+//   icon: formRules.email
+// };
+
+function createDefaultFormModel(): FormModel {
+  return {
+    id: null,
+    name: null,
+    description: null,
+    site: null,
+    icon: null
+  };
+}
+
+function handleUpdateFormModel(model: Partial<FormModel>) {
+  Object.assign(formModel, model);
+}
+
+function handleUpdateFormModelByModalType() {
+  const handlers: Record<ModalType, () => void> = {
+    add: () => {
+      const defaultFormModel = createDefaultFormModel();
+      handleUpdateFormModel(defaultFormModel);
+    },
+    edit: () => {
+      if (props.editData) {
+        handleUpdateFormModel(props.editData);
+      }
+    }
+  };
+
+  handlers[props.type]();
+}
+
+async function handleSubmit() {
+  // await formRef.value?.validate();
+  await addPage(formModel, props.type === 'add');
+  window.$message?.success('新增成功!');
+  closeModal();
+}
+
+watch(
+  () => props.visible,
+  newValue => {
+    if (newValue) {
+      handleUpdateFormModelByModalType();
+    }
+  }
+);
+</script>
+
+<style scoped></style>
---
+++ src/views/management/pageSetting/index.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="overflow-hidden">
+    <n-card title="网页管理" :bordered="false" class="h-full rounded-8px shadow-sm">
+      <div class="flex-col h-full">
+        <n-space class="pb-12px" justify="space-between">
+          <n-space>
+            <n-button type="primary" round @click="handleAddTable">
+              <icon-ic-round-plus class="mr-4px text-20px" />
+              新增
+            </n-button>
+            <n-button type="error" round>
+              <icon-ic-round-delete class="mr-4px text-20px" />
+              删除
+            </n-button>
+            <n-button type="success" round>
+              <icon-uil:export class="mr-4px text-20px" />
+              导出Excel
+            </n-button>
+          </n-space>
+          <n-space align="center" :size="18">
+            <n-button size="small" type="primary" @click="getTableData">
+              <icon-mdi-refresh class="mr-4px text-16px" :class="{ 'animate-spin': loading }" />
+              刷新表格
+            </n-button>
+            <column-setting v-model:columns="columns" />
+          </n-space>
+        </n-space>
+        <n-data-table
+          :columns="columns"
+          :data="tableData"
+          :loading="loading"
+          :pagination="pagination"
+          flex-height
+          class="flex-1-hidden"
+        />
+        <table-action-modal v-model:visible="visible" :type="modalType" :edit-data="editData" />
+      </div>
+    </n-card>
+  </div>
+</template>
+
+<script setup lang="tsx">
+import { reactive, ref } from 'vue';
+import type { Ref } from 'vue';
+import { NButton, NPopconfirm, NSpace, NTag } from 'naive-ui';
+import type { DataTableColumns, PaginationProps } from 'naive-ui';
+import { genderLabels, userStatusLabels } from '@/constants';
+import { getPages, deletePage } from '@/service';
+import { useBoolean, useLoading } from '@/hooks';
+import TableActionModal from './components/table-action-modal.vue';
+import type { ModalType } from './components/table-action-modal.vue';
+import ColumnSetting from './components/column-setting.vue';
+
+const { loading, startLoading, endLoading } = useLoading(false);
+const { bool: visible, setTrue: openModal } = useBoolean();
+
+const tableData = ref<ApiPageManagement.Page[]>([]);
+function setTableData(data: ApiPageManagement.Page[]) {
+  tableData.value = data;
+}
+
+async function getTableData() {
+  startLoading();
+  const { data } = await getPages();
+  if (data) {
+    setTimeout(() => {
+      setTableData(data);
+      endLoading();
+    }, 1000);
+  }
+}
+
+const columns: Ref<DataTableColumns<ApiPageManagement.Page>> = ref([
+  {
+    type: 'selection',
+    align: 'center'
+  },
+  {
+    key: 'id',
+    title: '序号',
+    align: 'center'
+  },
+  {
+    key: 'name',
+    title: '名称',
+    align: 'center'
+  },
+  {
+    key: 'description',
+    title: '描述',
+    align: 'center'
+  },
+  {
+    key: 'icon',
+    title: '图标',
+    align: 'center'
+  },
+  {
+    key: 'actions',
+    title: '操作',
+    align: 'center',
+    render: row => {
+      return (
+        <NSpace justify={'center'}>
+          <NButton size={'small'} onClick={() => handleEditTable(row.id)}>
+            编辑
+          </NButton>
+          <NPopconfirm onPositiveClick={() => handleDeleteTable(row.id)}>
+            {{
+              default: () => '确认删除',
+              trigger: () => <NButton size={'small'}>删除</NButton>
+            }}
+          </NPopconfirm>
+        </NSpace>
+      );
+    }
+  }
+]) as Ref<DataTableColumns<ApiPageManagement.Page>>;
+
+const modalType = ref<ModalType>('add');
+
+function setModalType(type: ModalType) {
+  modalType.value = type;
+}
+
+const editData = ref<ApiPageManagement.Page | null>(null);
+
+function setEditData(data: ApiPageManagement.Page | null) {
+  editData.value = data;
+}
+
+function handleAddTable() {
+  openModal();
+  setModalType('add');
+}
+
+function handleEditTable(rowId: number | null) {
+  const findItem = tableData.value.find(item => item.id === rowId);
+  if (findItem) {
+    setEditData(findItem);
+  }
+  setModalType('edit');
+  openModal();
+}
+
+function handleDeleteTable(rowId: number | null) {
+  deletePage([rowId]);
+  window.$message?.info(`点击了删除，rowId为${rowId}`);
+}
+
+const pagination: PaginationProps = reactive({
+  page: 1,
+  pageSize: 10,
+  showSizePicker: true,
+  pageSizes: [10, 15, 20, 25, 30],
+  onChange: (page: number) => {
+    pagination.page = page;
+  },
+  onUpdatePageSize: (pageSize: number) => {
+    pagination.pageSize = pageSize;
+    pagination.page = 1;
+  }
+});
+
+function init() {
+  getTableData();
+}
+
+// 初始化
+init();
+</script>
+
+<style scoped></style>
